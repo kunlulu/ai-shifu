bd4be685b17e9b3cc938d6faf633972bf2186243
kunlulu <kunlulu810@gmail.com>
2025-09-17 17:12:27 +0800

    feat: clear code

--- src/cook-web/src/app/c/[[...id]]/Components/ChatUi/NewChatComp.tsx
+++ src/cook-web/src/app/c/[[...id]]/Components/ChatUi/NewChatComp.tsx
@@ -3,59 +3,36 @@ import styles from './ChatComponents.module.scss';
 import {
   useEffect,
   forwardRef,
-  useImperativeHandle,
   useState,
   useContext,
   useRef,
   memo,
   useCallback,
-  useMemo,
 } from 'react';
 import { cn } from '@/lib/utils';
-import dynamic from 'next/dynamic';
-import { useTranslation } from 'react-i18next';
-
-import useMessages from './ForkChatUI/hooks/useMessages';
-import { Chat } from './ForkChatUI/components/Chat';
 import { useChatComponentsScroll } from './ChatComponents/useChatComponentsScroll';
 
 import { genUuid } from '@/c-utils/common';
-import ChatInteractionArea from './ChatInput/ChatInteractionArea';
 import { AppContext } from '@/c-components/AppContext';
 
 import { useCourseStore } from '@/c-store/useCourseStore';
 import {
   LESSON_STATUS_VALUE,
-  INTERACTION_TYPE,
-  INTERACTION_OUTPUT_TYPE,
-  RESP_EVENT_TYPE,
-  CHAT_MESSAGE_TYPE,
 } from '@/c-constants/courseConstants';
 
 import { useUserStore } from '@/store';
 import { fixMarkdownStream } from '@/c-utils/markdownUtils';
 import PayModal from '../Pay/PayModal';
-// TODO: FIXME
-// import LoginModal from '../Login/LoginModal';
 import { useDisclosure } from '@/c-common/hooks/useDisclosure';
-
-import { tokenTool } from '@/c-service/storeUtil';
-import MarkdownBubble from './ChatMessage/MarkdownBubble';
 import { useTracking, EVENT_NAMES } from '@/c-common/hooks/useTracking';
 import PayModalM from '../Pay/PayModalM';
 // import { useTranslation } from 'react-i18next';
 import { useEnvStore } from '@/c-store/envStore';
-import { shifu } from '@/c-service/Shifu';
 import {
   events,
   EVENT_NAMES as BZ_EVENT_NAMES,
 } from '@/app/c/[[...id]]/events';
-import ActiveMessageControl from './ChatMessage/ActiveMessageControl';
-import { convertKeysToCamelCase } from '@/c-utils/objUtils';
 import { useShallow } from 'zustand/react/shallow';
-
-import logoColor120 from '@/c-assets/logos/logo-color-120.png';
-import { STUDY_PREVIEW_MODE } from '@/c-constants/study';
 import { StudyRecordItem, LikeStatus ,getRunMessage, SSE_INPUT_TYPE, getLessonStudyRecord, PREVIEW_MODE, SSE_OUTPUT_TYPE, SYS_INTERACTION_TYPE} from '@/c-api/studyV2';
 import { ContentRender, OnSendContentParams } from 'markdown-flow-ui';
 import InteractionBlock from './InteractionBlock';
@@ -75,12 +52,11 @@ interface SSEParams {
   reload_generated_block_bid?: string;
 }
 
-export const NewChatComponents = forwardRef<any, any>(
-  (
+export const NewChatComponents = (
     {
       className,
       lessonUpdate,
-      onGoChapter = () => {},
+      onGoChapter,
       chapterId,
       lessonId,
       onPurchased,
@@ -88,7 +64,6 @@ export const NewChatComponents = forwardRef<any, any>(
       updateSelectedLesson,
       preview_mode = PREVIEW_MODE.NORMAL,
     },
-    ref,
   ) => {
     // const { t } = useTranslation();
     const { trackEvent, trackTrailProgress } = useTracking();
@@ -354,7 +329,6 @@ export const NewChatComponents = forwardRef<any, any>(
       if (!chapterId) {
         return;
       }
-      console.log('useEffect=====', chapterId, loadedChapterId);
       if (loadedChapterId === chapterId) {
         return;
       }
@@ -458,7 +432,7 @@ export const NewChatComponents = forwardRef<any, any>(
           onGoToNavigationNode,
         );
       };
-    }, [loadedChapterId, scrollToLesson, updateSelectedLesson]);
+    }, [loadedChapterId, scrollToLesson, updateSelectedLesson, refreshData]);
 
 
     return (
@@ -510,8 +484,8 @@ export const NewChatComponents = forwardRef<any, any>(
           ))}
       </div>
     );
-  },
-);
+}
+
 
 NewChatComponents.displayName = 'NewChatComponents';
 
