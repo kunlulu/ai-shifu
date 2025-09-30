e062ca65f4a80eb6aaacecbfbc085934edc682a8
kunlulu <kunlulu810@gmail.com>
2025-09-30 11:21:31 +0800

    feat: update mdf-ui

--- src/cook-web/src/app/c/[[...id]]/Components/ChatUi/ChatComponents.tsx
+++ src/cook-web/src/app/c/[[...id]]/Components/ChatUi/ChatComponents.tsx
@@ -230,7 +230,6 @@ export const ChatComponents = forwardRef<any, any>(
     });
 
     const { userInfo, mobileStyle } = useContext(AppContext);
-    console.log('ChatComponents mobileStyle',userInfo, mobileStyle);
     const chatRef = useRef(null);
 
     const { updateResetedChapterId } = useCourseStore(
